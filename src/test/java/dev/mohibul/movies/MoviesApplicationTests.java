eac7f05e5935591184e611af480179ffec451f18
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2023-08-17 11:04:56 +0600

    first commit

---
+++ src/test/java/dev/mohibul/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mohibul.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
